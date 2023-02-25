1c8ee399df4f2559d3aef607f4e6c9e493d560fe
Fabian Yesith Garcia Jurado <fygarciaj@gmail.com>
2023-02-25 14:21:19 -0500

    arranque de interface

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kivyBatteryReport
+
 Report Battery Status to Rest Api
+
+No es funcional, solo estoy probando desarrollo de aplicaciones android con kivy y python.
+
+No recomiendo utilizarlo de ningun modo.
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-import builtins
 from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.lang import Builder
+from kivy.utils import platform
 
 
-Window.size= (600, 800)
+if platform=='android':
+    from android.permissions import request_permissions, Permission
+    request_permissions([Permission.INTERNET])
 
-Builder.load_file('kvs/main.kv')
+if platform != "android":
+    Window.size = (300, 600)
+
+Builder.load_file('kvs/main.kv')  
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class MainScreen(Screen):
+    pass
 
 class MyApp(MDApp):
+    def __init__(self, **kwargs) -> None:
+        self.title="Battery Status"
+        super().__init__(**kwargs)
+    
     def build(self):
-        return 
+        self.wm = WindowManager()
+        self.theme_cls.theme_style = 'Light'
+        self.theme_cls.primary_palete = 'Teal'
+        screens = [
+            MainScreen(name='main'),
+        ]
+        for screen in screens:
+            self.wm.add_widget(screen)
+        return self.wm
 
 if __name__=="__main__":
     MyApp().run()
